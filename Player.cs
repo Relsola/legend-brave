f33b9af4c98fd5301e94ad882131112f6a83b590
Relsola <sola001018@gmail.com>
2025-11-19 22:32:24 +0800

    相机

--- Player.cs
+++ Player.cs
@@ -2,59 +2,57 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-    private Variant gravity = ProjectSettings.GetSetting("physics/2d/default_gravity");
-    private const float RunSpeed = 200.0f;
-    private const float JumpVelocity = -300.0f;
+	private Variant gravity = ProjectSettings.GetSetting("physics/2d/default_gravity");
+	private const float RunSpeed = 200.0f;
+	private const float JumpVelocity = -300.0f;
 
-    private Sprite2D _sprite2D;
-    private AnimationPlayer _animationPlayer;
+	private Sprite2D _sprite2D;
+	private AnimationPlayer _animationPlayer;
 
-    public override void _Ready()
-    {
-        _sprite2D = GetNode<Sprite2D>("Sprite2D");
-        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-    }
+	public override void _Ready()
+	{
+		_sprite2D = GetNode<Sprite2D>("Sprite2D");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        // 控制台打印消息
+	public override void _PhysicsProcess(double delta)
+	{
+		var velocity = Velocity;
+		var direction = Input.GetAxis("move_left", "move_right");
 
-        var velocity = Velocity;
-        var direction = Input.GetAxis("move_left", "move_right");
+		velocity.Y += (float)gravity * (float)delta;
+		velocity.X = direction * RunSpeed;
 
-        velocity.Y += (float)gravity * (float)delta;
-        velocity.X = direction * RunSpeed;
+		if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+		{
+			velocity.Y = JumpVelocity;
+		}
 
-        if (IsOnFloor() && Input.IsActionJustPressed("jump"))
-        {
-            velocity.Y = JumpVelocity;
-        }
-
-        if (IsOnFloor())
-        {
-            if (Mathf.IsZeroApprox(direction))
-            {
-                _animationPlayer.Play("idle");
-            }
-            else
-            {
-                _animationPlayer.Play("running");
-                _sprite2D.FlipH = direction < 0;
-            }
-        }
-        else
-        {
-            _animationPlayer.Play("jump");
-        }
+		if (IsOnFloor())
+		{
+			if (Mathf.IsZeroApprox(direction))
+			{
+				_animationPlayer.Play("idle");
+			}
+			else
+			{
+				_animationPlayer.Play("running");
+				_sprite2D.FlipH = direction < 0;
+			}
+		}
+		else
+		{
+			_animationPlayer.Play("jump");
+		}
 
 
-        if (!Mathf.IsZeroApprox(direction))
-        {
-            _sprite2D.FlipH = direction < 0;
-        }
+		if (!Mathf.IsZeroApprox(direction))
+		{
+			_sprite2D.FlipH = direction < 0;
+		}
 
 
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
---
+++ World.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class World : Node2D
+{
+
+    private TileMapLayer _tileMapLayer;
+    private Camera2D _camera2D;
+
+    public override void _Ready()
+    {
+        _tileMapLayer = GetNode<TileMapLayer>("TileMapLayer");
+        _camera2D = GetNode<Camera2D>("Player/Camera2D");
+
+        var rect = _tileMapLayer.GetUsedRect();
+        var tileSize = _tileMapLayer.TileSet.TileSize;
+
+        _camera2D.LimitTop = rect.Position.Y * tileSize.Y;
+        _camera2D.LimitRight = rect.End.X * tileSize.X;
+        _camera2D.LimitBottom = rect.End.Y * tileSize.Y;
+        _camera2D.LimitLeft = rect.Position.X * tileSize.X;
+
+        _camera2D.ResetSmoothing();
+    }
+}
